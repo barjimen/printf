b11c4afcde05cd1656c75eae33f955d1a7e22dc1
barjimen <barby.jimenez17@gmail.com>
2023-08-18 19:38:19 +0200

    midiendo hexa

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main()
     int medida1;
     int medida2;
 
-    medida1 = ft_printf("%p\n",NULL);
-    medida2 = printf("%p\n",NULL);
+    medida1 = ft_printf("%x\n",275555555);
+    medida2 = printf("%x\n",275555555);
     
     printf("medida 1 -> %d\n medida 2 -> %d\n", medida1, medida2);
     return 0;
