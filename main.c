5121b09d9afadbd78b6129b2d4493b04983a3f18
barjimen <barby.jimenez17@gmail.com>
2023-08-18 18:18:54 +0200

    actuali

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main()
     int medida1;
     int medida2;
 
-    medida1 = ft_printf("-> %c\n", '*');
-    medida2 = printf("-> %c\n",'*');
+    medida1 = ft_printf("%c\n",'c');
+    medida2 = printf("%c\n",'c');
     
     printf("medida 1 -> %d\n medida 2 -> %d\n", medida1, medida2);
     return 0;
